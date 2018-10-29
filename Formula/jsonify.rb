fdfdec104934c6fb5cc56157070469ede38431df
Felix Geller <fgeller@gmail.com>
2018-10-29 18:27:53 +0100

    updates jsonify after fixing release

--- Formula/jsonify.rb
+++ Formula/jsonify.rb
@@ -3,7 +3,7 @@ class Jsonify < Formula
   homepage "https://github.com/fgeller/jsonify"
   url "https://github.com/fgeller/jsonify/releases/download/v1.0.0/jsonify-v1.0.0-darwin-amd64.txz"
   version "v1.0.0"
-  sha256 "6afbc3d97b063b0723cd0e779ac70c93e73b36d0d52752cd8d61e7f644583fe4"
+  sha256 "39827788107969789281e38e048349ac2f46a2c03817d73b61685aa3fa8491bb"
 
   def install
     bin.install "jsonify"
