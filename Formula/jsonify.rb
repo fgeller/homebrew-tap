eb0b1d0dc15d1235a6e39f7367e24e82827b88a3
Felix Geller <fgeller@gmail.com>
2022-03-03 07:58:57 +0100

    updates jsonify to v1.1.0

--- Formula/jsonify.rb
+++ Formula/jsonify.rb
@@ -1,9 +1,9 @@
 class Jsonify < Formula
   desc "Create JSON objects quickly"
   homepage "https://github.com/fgeller/jsonify"
-  url "https://github.com/fgeller/jsonify/releases/download/v1.0.0/jsonify-v1.0.0-darwin-amd64.txz"
-  version "v1.0.0"
-  sha256 "39827788107969789281e38e048349ac2f46a2c03817d73b61685aa3fa8491bb"
+  url "https://github.com/fgeller/jsonify/releases/download/v1.1.0/jsonify-v1.1.0-darwin-amd64.txz"
+  version "v1.1.0"
+  sha256 "1466be6c3a9a078f41fce284fecf5bd6ffe0251655b5659ea8ac849230b814a2"
 
   def install
     bin.install "jsonify"
