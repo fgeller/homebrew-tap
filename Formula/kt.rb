027a556c9c2ffb7a6fa9494a1759ea0d1a62ef37
Felix Geller <fgeller@gmail.com>
2022-03-03 07:40:20 +0100

    updates kt to v13.1

--- Formula/kt.rb
+++ Formula/kt.rb
@@ -1,9 +1,9 @@
 class Kt < Formula
   desc "Kafka command line tools that likes JSON"
   homepage "https://github.com/fgeller/kt"
-  url "https://github.com/fgeller/kt/releases/download/v12.1.0/kt-v12.1.0-darwin-amd64.txz"
-  version "v12.1.0"
-  sha256 "19b5f6a24d1a99d6c9d6009c1bf9b9d84b69999a8ff3d06639e671208161ffe4"
+  url "https://github.com/fgeller/kt/releases/download/v13.1.0/kt-v13.1.0-darwin-amd64.txz"
+  version "v13.1.0"
+  sha256 "2d0476a5a6a72bccc2b12f3551619d6ea54500e9c9c8f3660dd5de14066e9bf4"
 
   def install
     bin.install "kt"
